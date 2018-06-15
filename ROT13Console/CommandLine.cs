7740532dbc269b6f7cf4e889f6961b517b1b911e
Valentin Iliescu <viliescu@hotmail.com>
2018-06-14 17:54:16 -0700

    Implement bare-bones CommandLine infrastructure class

---
+++ ROT13Console/CommandLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ROT13Console
+{
+    public class CommandLine
+    {
+        public string Arg => Environment.GetCommandLineArgs().Skip(1).FirstOrDefault();
+
+        public static CommandLine Create()
+        {
+            return new CommandLine();
+        }
+    }
+}
---
+++ ROT13ConsoleTests/CommandLineTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ROT13Console;
+
+namespace ROT13ConsoleTests
+{
+    [TestClass]
+    public class CommandLineTests
+    {
+        [TestMethod]
+        [TestCategory("Integration")]
+        public void ProvidesFirstCommandLineArgument()
+        {
+            //In .NET, you cannot modify command line arguments for the current process
+            var cli = CommandLine.Create();
+            var arg = cli.Arg;
+            arg.Should().NotBeNull();
+        }
+    }
+}
