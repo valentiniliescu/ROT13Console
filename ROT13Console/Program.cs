c6f20b9583167c3ca8f4b80ebf5c3a64d46b2b84
Valentin Iliescu <viliescu@hotmail.com>
2018-06-14 17:38:26 -0700

    Added unit test project; Added FluentAssertions NuGet; ReSharper clean-up

--- ROT13Console/Program.cs
+++ ROT13Console/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ROT13Console
+﻿namespace ROT13Console
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
         }
     }
